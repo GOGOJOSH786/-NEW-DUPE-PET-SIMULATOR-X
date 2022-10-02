9490e51f1bfb9b57ebfd064e8e3bf16d65bd3fb6
GOGOJOSH786 <75281246+GOGOJOSH786@users.noreply.github.com>
2022-10-02 12:56:56 +0100

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:httpget("https://raw.githubusercontent.com/GOGOJOSH786/-NEW-DUPE-PET-SIMULATOR-X/main/loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/GOGOJOSH786/-NEW-DUPE-PET-SIMULATOR-X/main/loader.lua"))();
